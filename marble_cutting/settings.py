491472bf06bbb443a284562608f490391f329e39
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2025-09-20 23:23:09 +0530

    created env file

--- marble_cutting/settings.py
+++ marble_cutting/settings.py
@@ -12,22 +12,27 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables
+env_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path=env_path)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xg!k#00*2)vuscw5*2jok5mee&r7gd00mg+et6!sb_139-o2=@'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = True   # For Production
-
-ALLOWED_HOSTS = ['*']
+DEBUG = os.getenv('DEBUG') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
